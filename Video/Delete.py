555f894163920c685c8249aba67355c904edd622
C.Whale <jingyubest101@gmail.com>
2025-02-10 13:19:40 +0800

    删除文件夹中用来解压的Video文件

--- Video/Delete.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import os
-def delete():
-    shutil.rmtree('Camera_Output')
-    os.mkdir('Camera_Output')
-    shutil.rmtree('Histogram')
-    os.mkdir('Histogram')
-    shutil.rmtree('runs')   
-    os.mkdir('runs')
--- Video/OsComm.py
+++
@@ -1,3 +0,0 @@
-import os
-def OsComm():
-    os.system('yolo predict model=./yolov8n.pt source=./Histogram')
